9e6efd9232e97918c05474c5422c7582c2d4a7c6
Berger Benjamin <amrun@dungeonfire.ch>
2013-02-11 00:22:46 +0100

    Initial commit for AbewoAddressBook

---
+++ AbewoAddressBook/Entities/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbewoAddressBook.Entities
+{
+	class Person
+	{
+		public bool Gender { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string Street { get; set; }
+		public string StreetNr { get; set; }
+		public string Plz { get; set; }
+		public string City { get; set; }
+		public string Email { get; set; }
+		public Company Company { get; set; }
+
+	}
+}
---
+++ AbewoAddressBook/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Data.Entity;
+
+using AbewoAddressBook.Entities;
+
+namespace AbewoAddressBook
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow: Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		public void SearchField_TextChanged( object sender, TextChangedEventArgs e )
+		{
+
+			List<Person> personList = new List<Person>();
+			Person p1 = new Person();
+			p1.Name = "Hans Meier";
+			p1.Email = "hansmeier@meier.ch";
+
+			Person p2 = new Person();
+			p2.Name = "Joggeli Müller";
+			p2.Email = "joggi@meier.ch";
+
+			personList.Add( p1 );
+			personList.Add( p2 );
+			try
+			{
+				ResultList.ItemsSource = personList;
+			}
+			catch { 
+			}
+		}
+
+	}
+}
